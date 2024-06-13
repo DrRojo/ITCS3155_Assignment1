53a64dc590feecb46a457246b0e564e64c5f7420
DrRojo <christopher.d.berns@gmail.com>
2024-06-13 16:27:32 -0400

    updating

--- main.py
+++ main.py
@@ -53,11 +53,23 @@ class SandwichMachine:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
+        total = int(input("How many large dollars? ")) * 1.00
+        total += int(input("How many half dollars? ")) * 0.50
+        total += int(input("How many quarters? ")) * 0.25
+        total += int(input("How many nickels? ")) * 0.05
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            if change > 0:
+                print(f"Here is ${change} in change. ")
+            return True
+        else:
+            print("Sorry, that is not enough money.  Money refunded. ")
+            return False
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
            Hint: no output"""
